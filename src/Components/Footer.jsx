144da8d25e48372a56d5f34168aea04f822ddf0b
Narenthar <80049709+Narenthar@users.noreply.github.com>
2022-04-16 01:08:11 +0530

    footer style

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,26 +1,27 @@
 import React from "react";
+import "../Styles/Footer.css"
 
 export const Footer = () => {
 const year = new Date().getFullYear();
 return (
-    <div>
-    <div>
-        <p>Contact me</p>
-    </div>
-    <div>
-        <div>
+    <div className="footer-container">
+    <div className="footer-card">
+        <p className="footer-header">Contact me</p>
+    
+    <div className="footer-col">
+        <div className="footer-row">
         <i className="fas fa-envelope" />
-        <a href="mailto:narenthar31@gmail.com">narenthar31@gmail.com</a>
+        <a className="mailid" href="mailto:narenthar31@gmail.com">narenthar31@gmail.com</a>
         </div>
-        <div>
+        <div className="footer-row">
         <i className="fas fa-mobile" />
         <a href="tel:+919962988080">
-            <p>  (+91)9962988080</p>
+            <p className="mobile">  (+91)9962988080</p>
           </a>
         </div>
     </div>
-    <p>Follow me</p>
-    <div>
+    <p className="footer-header">Follow me</p>
+    <div className="footer-row">
         <a
         href="https://www.instagram.com/naren_thar_/"
         target="_blank"
@@ -46,8 +47,10 @@ return (
         <i className="fab fa-github"></i>
         </a>
     </div>
-    <div>
-        <p>Narenthar Chidambaram ⓒ {year}</p>
+   
+    <div className="copyright-card">
+        <p className="copyright-text">Narenthar Chidambaram ⓒ {year}</p>
+    </div>
     </div>
     </div>
 );
