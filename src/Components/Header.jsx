52fece655880cea1ea707a6d0b5899c71e7997bf
Narenthar <80049709+Narenthar@users.noreply.github.com>
2022-04-17 02:30:50 +0530

    scroll

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,23 +1,43 @@
 import React from "react";
-// import { Nav, Navbar, NavbarBrand, NavItem, Container } from "reactstrap";
 import "../Styles/Header.css";
+import { Link } from "react-scroll";
 
 export const Header = () => {
   return (
     <nav className="navbar">
- <div className="logo"><i className="fa fa-home"></i></div>
- <ul className="nav-links">
- <input type="checkbox" id="checkbox_toggle" />
- <label htmlFor="checkbox_toggle" className="hamburger">&#9776;</label>
- <div className="menu">
- <li>Projects</li>
- <li>Skills</li>
- <li>
- Resume
- </li>
- <li>Contact</li>
- </div>
- </ul>
- </nav>
+      <div className="logo">
+        <Link to="about" smooth={true} offset={-70} duration={500}>
+          <i className="fa fa-home"></i>
+        </Link>
+      </div>
+      <ul className="nav-links">
+        <input type="checkbox" id="checkbox_toggle" />
+        <label htmlFor="checkbox_toggle" className="hamburger">
+          &#9776;
+        </label>
+        <div className="menu">
+          <li>
+            <Link to="projects" smooth={true} offset={-70} duration={500}>
+              <span className="nav-name">Projects</span>
+            </Link>
+          </li>
+          <li>
+            <Link to="skills" smooth={true} offset={-70} duration={500}>
+              <span className="nav-name">Skills</span>
+            </Link>
+          </li>
+          <li>
+            <a className="nav-name" href = "">
+              <span >Resume</span>
+            </a>
+          </li>
+          <li>
+            <Link to="footer" smooth={true} offset={80} duration={500}>
+              <span className="nav-name">Contact</span>
+            </Link>
+          </li>
+        </div>
+      </ul>
+    </nav>
   );
 };
